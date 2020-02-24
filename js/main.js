9dbc0f237d030fa1c57081c247b6578a420e6113
santrancisco <san.tran@ebfe.pw>
2020-02-24 11:52:33 +1100

    updated latest code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 
 var soundlib = {
+    // WEATHER ambience sound
 	"windy":{"url":"https://actions.google.com/sounds/v1/weather/strong_wind.ogg","v":1.0, "loop":true},
     "rain":{"url":"https://actions.google.com/sounds/v1/weather/light_rain.ogg","v":1.0, "loop":true},
-    "heavy_rain":{"url":"https://actions.google.com/sounds/v1/weather/rain_heavy_loud.ogg","v":1.0, "loop":true},
+    "heavy_rain":{"url":"https://actions.google.com/sounds/v1/weather/rain_heavy_loud.ogg","v":0.5, "loop":true},
     "thunder_storm":{"url":"https://actions.google.com/sounds/v1/weather/thunderstorm_long.ogg","v":1.0, "loop":true},
     "warm_breeze":{"url":"https://actions.google.com/sounds/v1/weather/leaves_russle_on_tree.ogg","v":1.0, "loop":true},
     "distant_thunder":{"url":"https://actions.google.com/sounds/v1/weather/distant_thunder.ogg","v":1.0, "loop":true},
-    // BELOW ARE fun ambiance sounds
+    // some fun ambiance sounds
     "fire":{"url":"https://actions.google.com/sounds/v1/ambiences/fire.ogg","v":0.8,"loop":true},
     "daylight_fire":{"url":"https://actions.google.com/sounds/v1/ambiences/daytime_forrest_bonfire.ogg","v":0.8, "loop":true},
     "coffee_shop":{"url":"https://actions.google.com/sounds/v1/ambiences/coffee_shop.ogg","v":0.8, "loop":true},
+    "outdoor_busy_people":{"url":"https://actions.google.com/sounds/v1/ambiences/outdoor_event_background_noise.ogg","v":0.5, "loop":true},
+    "small_outdoor_marketplace":{"url":"https://actions.google.com/sounds/v1/ambiences/small_outdoor_marketplace.ogg","v":0.6, "loop":true},
+
+    // Environment specific ambiance sounds
     "farm_animal":{"url":"https://actions.google.com/sounds/v1/ambiences/barnyard_with_animals.ogg","v":0.8, "loop":true},
     "quiet_night":{"url":"https://actions.google.com/sounds/v1/ambiences/july_night.ogg","v":0.8, "loop":true},
     "fresh_morning_farm":{"url":"https://actions.google.com/sounds/v1/ambiences/morning_farm.ogg","v":0.8, "loop":true},
@@ -18,14 +23,36 @@ var soundlib = {
     "cricket_night":{"url":"https://actions.google.com/sounds/v1/ambiences/outside_night.ogg","v":0.8, "loop":true},
     "warm_afternoon":{"url":"https://actions.google.com/sounds/v1/ambiences/warm_afternoon_outdoors.ogg","v":0.8, "loop":true},
     "warm_night":{"url":"https://actions.google.com/sounds/v1/ambiences/warm_evening_outdoors.ogg","v":0.8, "loop":true},
-    "outdoor_busy_people":{"url":"https://actions.google.com/sounds/v1/ambiences/outdoor_event_background_noise.ogg","v":0.3, "loop":true},
-    // WEAPON - not loop
-    "rifle_echo_long":{"url":"https://actions.google.com/sounds/v1/weapons/rifle_shot_echo.ogg","v":0.5, "loop":false},
-    "":{"url":"","v":0.8, "loop":true},
-    "":{"url":"","v":0.8, "loop":true},
-    "":{"url":"","v":0.8, "loop":true},
+    "distant_thunder":{"url":"https://actions.google.com/sounds/v1/weather/distant_thunder.ogg","v":0.8, "loop":true},
+    "rain_on_roof":{"url":"https://actions.google.com/sounds/v1/weather/rain_on_roof.ogg","v":0.8, "loop":true},
+    "rumbling_wind":{"url":"https://actions.google.com/sounds/v1/weather/rumbling_wind.ogg","v":0.6, "loop":true},
+    
+    // WATER related
+    "waves_crashing_on_rock_beach":{"url":"https://actions.google.com/sounds/v1/water/waves_crashing_on_rock_beach.ogg","v":0.6, "loop":true},
+    "water_running_by":{"url":"https://actions.google.com/sounds/v1/water/water_running_by.ogg","v":1.0, "loop":true},
+    "water_lapping_wind":{"url":"https://actions.google.com/sounds/v1/water/water_lapping_wind.ogg","v":0.8, "loop":true},
+    "small_stream_flowing":{"url":"https://actions.google.com/sounds/v1/water/small_stream_flowing.ogg","v":1.0, "loop":true},
+    "sea_and_seagul":{"url":"https://bigsoundbank.com/UPLOAD/ogg/0267.ogg","v":0.8, "loop":true},
+    "sea_wave":{"url":"https://freesound.org/data/previews/197/197714_7037-lq.mp3","v":0.8, "loop":true},
+
+    // SPACE/Science fiction or something we can use for that theme
+    "outer_space":{"url":"https://actions.google.com/sounds/v1/science_fiction/outer_space.ogg","v":0.8, "loop":true},
+    "sci_fi_vortex":{"url":"https://actions.google.com/sounds/v1/science_fiction/sci_fi_vortex.ogg","v":0.3, "loop":true},
+    "forboding_resonance":{"url":"https://actions.google.com/sounds/v1/science_fiction/forboding_resonance.ogg","v":0.8, "loop":true},
+    "metal_construction_background":{"url":"https://actions.google.com/sounds/v1/tools/metal_construction_background.ogg","v":0.6, "loop":true},
+    // TRIGGER - not loop
+    "rifle_echo_long":{"url":"https://actions.google.com/sounds/v1/weapons/rifle_shot_echo.ogg","v":0.7, "loop":false},
+    "thunder_crack":{"url":"https://actions.google.com/sounds/v1/weather/thunder_crack.ogg","v":0.8, "loop":false},
+    "human_fart":{"url":"https://actions.google.com/sounds/v1/human_voices/human_fart.ogg","v":0.8, "loop":false},
+    "dumpster_door_hit":{"url":"https://actions.google.com/sounds/v1/impacts/dumpster_door_hit.ogg","v":0.9, "loop":false},
+    "small_glass_pane_shatter":{"url":"https://actions.google.com/sounds/v1/impacts/small_glass_pane_shatter.ogg","v":0.8, "loop":false},
+    "drill_motor_dopper":{"url":"https://actions.google.com/sounds/v1/tools/drill_motor_dopper.ogg","v":0.8, "loop":false},
+    
+    
+    
     "":{"url":"","v":0.8, "loop":true},
 };
+// LoL someone at google talk over this recording for keyboard typing sound (something about closing bathroom door) https://actions.google.com/sounds/v1/office/keyboard_typing_fast.ogg
 
 currentlanguage="en";
 
--- js/scenarios.js
+++ js/scenarios.js
@@ -1,80 +1,134 @@
 
 var scenarios = [
     {
-        "description":{
-            "en":"It is 2045, a war broke out and your family is wondering through the apocalyptic land.",
-            "vn":"Bây giờ là năm 2045, một cuộc chiến tranh sảy ra và gia đình bạn đang phải mạo hiểm đi qua những thành phố đổ nát."
+      "description": {
+        "en": "You are stuck in the middle of the sea on a boat! (Get your kids to jump on a sofa or a bed!)",
+        "vn": "Các con đang lênh đênh trên biển trên một con thuyền! (Bạn hãy lôi các bé lên giường hay sofa để giả vờ!)"
+      },
+      "stages": [
+        {
+          "time": 60,
+          "read": {
+            "en": "After the storm, you are floating on a lifeboat. It's a bright new day - time to catch some fishes!",
+            "vn": "Sau một cơn bão, các bạn đang tắc ở trên thuyền cứu nạn. Hôm nay trời đã rạng, chúng mình phải bắt cá để ăn thôi!"
+          },
+          "sounds": {
+            "water_running_by": 0
+          }
         },
-        "stages":[
-            {
-                "time": 50,
-                "read":{
-                    "en":"You are wondering through an abandoned town but there is no sign of living. A storm is approaching in and you must find food and shelter.",
-                    "vn":"Bạn đang đi qua một ngôi làng bỏ hoang không một bóng người. Một cơn bão sắp ập tới, bạn cần tìm chỗ chú và đồ ăn."
-                },
-                "sounds":{
-                    "windy":0,
-                    "rifle_echo_long":46
-                }
-            },
-            {
-            	"time": 60,
-                "read": {
-                    "en":"While sitting around the campfire you heard a gun shoot and small noises outside the door. You take a look and found a wounded and exhausted dog outside.",
-                    "vn":"Đang ngồi bên lửa trại, bạn nghe thấy tiếng súng rồi có tiếng động nhỏ ở ngoài cửa. Nhìn ra ngoài, bạn phát hiện có một chú chó bị thương và nhìn rất mệt mỏi."
-                },
-                "sounds":{
-                    "thunder_storm":0,
-                    "fire":0
-                }
-             },
-             {
-            	"time": 60,
-                "read": {
-                    "en":"Your friends are exhausted and they need food. The sky is clearing and It's time to go out and search for food.",
-                    "vn":"Những người đi cùng bạn đã rất mệt mỏi. Mưa đã tạnh, đã đến lúc bạn đi tìm đồ ăn cho mọi người. "
-                },
-                "sounds":{
-                    "warm_night":0
-                }
-         	}
-        ],
-        "exit": {
-            "en":"Feeling fresh and ready, you can finally leave the town!",
-            "vn":"Bạn đã cảm thấy khỏe mạnh lên nhiều và có thể rời khỏi ngôi làng này."
+        {
+          "time": 60,
+          "read": {
+            "en": "Dark clouds quickly gather, time to prepare for the storm.",
+            "vn": "Bầu trời bỗng trở nên xám xịt! Đã đến lúc các bạn chuẩn bị đón bão!"
+          },
+          "sounds": {
+            "distant_thunder": 0
+          }
+        },
+        {
+          "time": 60,
+          "read": {
+            "en": "The rain starts pouring down. The waves are clashing against your boat. Everybody need to hold tight!",
+            "vn": "Mưa đang trút xuống, Sóng biển đang làm thuyền chao đảo! Tất cả mọi người phải giữ thật chặt lấy thuyền!"
+          },
+          "sounds": {
+            "heavy_rain": 0,
+            "sea_wave": 0,
+            "thunder_crack": 40
+          }
+        },
+        {
+          "time": 60,
+          "read": {
+            "en": "Finally the rain has stopped. You can hear some birds chirping! This must mean that there is land nearby!",
+            "vn": "Bão đã tạnh. Bạn bất ngờ nghe thấy tiếng chim hải âu! Chắc chắn gần đây có đất liền! Chúng ta tìm đất liền thôi!"
+          },
+          "sounds": {
+            "sea_and_seagul": 0
+          }
         }
+      ],
+      "exit": {
+        "en": "With amazing effort, you and your team have managed to reach the shore. Well done!",
+        "vn": "Với sự cố gắng hết mình, bạn đã đến được bờ biển ! Chúc mừng các bạn!!!"
+      }
     },
     {
-        "description":{
-            "en":"It is 1940, In the middle of world war 2, you and your family managed to escape to a farm.",
-            "vn":"1940 - Chiến tranh thế giời thư 2 đang xảy ra. Gia đình bạn đã chạy trốn được đến một nông trại bỏ hoang."
+      "description": {
+        "en": "It is 2045, a war broke out and your family is wondering through the apocalyptic land.",
+        "vn": "Bây giờ là năm 2045, một cuộc chiến tranh sảy ra và gia đình bạn đang phải mạo hiểm đi qua những thành phố đổ nát."
+      },
+      "stages": [
+        {
+          "time": 50,
+          "read": {
+            "en": "You are wondering through an abandoned town but there is no sign of living. A storm is approaching in and you must find food and shelter.",
+            "vn": "Bạn đang đi qua một ngôi làng bỏ hoang không một bóng người. Một cơn bão sắp ập tới, bạn cần tìm chỗ chú và đồ ăn."
+          },
+          "sounds": {
+            "windy": "0ưng",
+            "rifle_echo_long": 46
+          }
         },
-        "stages":[
-            {
-                "time": 60,
-                "read":{
-                    "en":"Everyone stretch their legs and get out of the tent. It is important to keep your self healthy. You gather everyone and start your daily exercise.",
-                    "vn":"Trời đã sáng, đã đến lúc chúng ta tập thể dục buổi sáng. Trong môi trường loạn lạc như vậy, việc giữ gìn sức khỏe là rất quan trọng!"
-                },
-                "sounds":{
-                    "chicken_morning_farm":0
-                }
-            },
-            {
-            	"time": 60,
-                "read": {
-                    "en":"Dark clouds gather quickly. It's time to head back into the barn and wait for the rain to finish.",
-                    "vn":"Bầu trời bỗng trở nên xám xịt! Đã đến lúc chúng ta quay vào nhà và chờ cho mưa tạnh."
-                },
-                "sounds":{
-                    "heavy_rain":0,
-                    "thunder_storm":30
-                }
-         	}
-        ],
-        "exit":  {
-            "en":"Feeling fresh and ready, you can finally leave the farm.",
-            "vn":"Bạn đã cảm thấy khỏe mạnh lên nhiều và có thể rời khỏi nơi này."
+        {
+          "time": 60,
+          "read": {
+            "en": "While sitting around the campfire you heard a gun shoot and small noises outside the door. You take a look and found a wounded and exhausted dog outside.",
+            "vn": "Đang ngồi bên lửa trại, bạn nghe thấy tiếng súng rồi có tiếng động nhỏ ở ngoài cửa. Nhìn ra ngoài, bạn phát hiện có một chú chó bị thương và nhìn rất mệt mỏi."
+          },
+          "sounds": {
+            "thunder_storm": 0,
+            "fire": 0
+          }
+        },
+        {
+          "time": 60,
+          "read": {
+            "en": "Your friends are exhausted and they need food. The sky is clearing and It's time to go out and search for food.",
+            "vn": "Những người đi cùng bạn đã rất mệt mỏi. Mưa đã tạnh, đã đến lúc bạn đi tìm đồ ăn cho mọi người. "
+          },
+          "sounds": {
+            "warm_night": 0
+          }
         }
+      ],
+      "exit": {
+        "en": "Feeling fresh and ready, you can finally leave the town!",
+        "vn": "Bạn đã cảm thấy khỏe mạnh lên nhiều và có thể rời khỏi ngôi làng này."
+      }
+    },
+    {
+      "description": {
+        "en": "It is 1940, In the middle of world war 2, you and your family managed to escape to a farm.",
+        "vn": "1940 - Chiến tranh thế giời thư 2 đang xảy ra. Gia đình bạn đã chạy trốn được đến một nông trại bỏ hoang."
+      },
+      "stages": [
+        {
+          "time": 60,
+          "read": {
+            "en": "Everyone stretch their legs and get out of the tent. It is important to keep your self healthy. You gather everyone and start your daily exercise.",
+            "vn": "Trời đã sáng, đã đến lúc chúng ta tập thể dục buổi sáng. Trong môi trường loạn lạc như vậy, việc giữ gìn sức khỏe là rất quan trọng!"
+          },
+          "sounds": {
+            "chicken_morning_farm": 0
+          }
+        },
+        {
+          "time": 60,
+          "read": {
+            "en": "Dark clouds gather quickly. It's time to head back into the barn and wait for the rain to finish.",
+            "vn": "Bầu trời bỗng trở nên xám xịt! Đã đến lúc chúng ta quay vào nhà và chờ cho mưa tạnh."
+          },
+          "sounds": {
+            "heavy_rain": 0,
+            "thunder_storm": 5
+          }
+        }
+      ],
+      "exit": {
+        "en": "Feeling fresh and ready, you can finally leave the farm.",
+        "vn": "Bạn đã cảm thấy khỏe mạnh lên nhiều và có thể rời khỏi nơi này."
+      }
     }
-]
+  ]
